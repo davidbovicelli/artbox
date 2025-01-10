9030612b1dd76bdaae8b2aa859474509c731c935
davidbovicelli <david.bovicelli@outlook.fr>
2025-01-10 16:55:16 +0100

    correction bug liens acceuil

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
     <a href="index.html"><img src="img/logo.png" alt="Logo Artbox" id="logo"></a>
     <nav>
         <ul>
-            <li><a href="index.html">Accueil</a></li>
+            <li><a href="index.php">Accueil</a></li>
         </ul>
     </nav>
 </header>
